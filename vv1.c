0f82c2618d2329f6f721360801a6c5521ca63bf1
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-01-08 10:28:27 +0000

    Create vv1.c

---
+++ vv1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ {
+ int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the given is positive number");
+}
+elseif(a<0)
+{
+printf("the given is negative number");
+}
+else
+{
+printf("the given is zero");
+}
+getch();
+}

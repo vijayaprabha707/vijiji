d446f47b3381bbdab903dc891f6060c66f1d18b4
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-01-08 10:34:22 +0000

    Create vv2.c

---
+++ vv2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+ int num;
+ printf("enter an integer");
+ scanf("%d",&num);
+ if(num%2==0)
+ {
+ printf("%d is even",num);
+ }
+ else
+ {
+ printf("%d is odd",num);
+ }
+return 0;
+}

469150f57dbfcfea2ef1717af938e4561afc417d
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-01-19 11:05:29 +0000

    Create vv19.c

---
+++ vv19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    fact=1;
+    printf("Enter an integer");
+    scanf("%d",&n);
+    if (n < 0)
+        printf("factorial of a negative number does not exist");
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+         fact=fact*i;
+        }
+        printf("Factorial of %d = %llu", n, fact);
+    }
+return 0;

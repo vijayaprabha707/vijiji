3f768227ac712cde9371b42d4bd447cf70996319
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-02-07 16:34:41 +0530

    Create vv43.c

---
+++ vv43.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+ char a[20], char b[50];
+ char c[20];
+ printf("enter the string");
+ gets();
+ printf("enter the string");
+ gets();
+  c=strcat(a,b);
+ printf("the concatenated string is%d",c);
+ }

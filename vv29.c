2e59ce53c26b91a7c96f8f9755570953c6eea2b9
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-02-19 16:08:46 +0530

    Create vv29.c

---
+++ vv29.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void  main()
+{
+int min,val=60,h,m;
+printf("enter the  min");
+scanf("%d",&min);
+if(min>val)
+{
+h=min/val;
+m=min%val;
+printf("%d%d",h,m);
+}
+else
+{
+printf("%d",minutes);
+}
+}

73b7745d6ec622a9d5526916f2a57df4a70eaa40
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-02-21 16:49:16 +0530

    Create nv66.c

---
+++ nv66.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+ int n,i,count=0;
+ printf("enter the number");
+ scanf("%d",&n);
+ for(i=1;i<n;i++)
+ {
+ if(n%i==0)
+ {
+  count++;
+  }
+  if(count==0)
+  {
+  printf("prime number");
+  }
+  else
+  {
+  printf("not a prime number");
+  }
+  }

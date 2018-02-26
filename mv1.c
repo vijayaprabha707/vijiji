32b0ba393db735e99b59f1dd5242bfacc71e3f62
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-02-26 16:17:25 +0530

    Create mv1.c

---
+++ mv1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,k,n;
+	char x[100],y[100];
+	scanf("%s",&x);
+	scanf("%s",&y);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==b[i])
+		{
+			printf("%c",a[i]);
+		}
+		else
+		{
+			break;
+		}
+	}
+	return 0;
+}

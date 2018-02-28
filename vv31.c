491cb830d037a0a849ca07192229c47910a502b2
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-02-28 15:54:32 +0530

    Create vv31.c

---
+++ vv31.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main()
+{
+    int i,c=0;
+    char a[i];
+    printf("enter the string");
+    scanf("%s",&a[i]);
+    for(i=0;a[i]!='\0';i++)
+    {
+        count++;
+    }
+    return c;
+}

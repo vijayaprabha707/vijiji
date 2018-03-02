43e1a9d74a6f464025bedee191f21703b093c442
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-03-02 15:54:51 +0530

    Create vv52.c

---
+++ vv52.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case:1
+printf("one");
+break;
+case2:
+printf("two");
+break;
+case3:
+printf("three");
+break;
+case4:
+printf("four");
+break;
+default:
+printf("not present enter number");
+break;
+}
+}

430f7a7b08f1394ae13f0c5fa345dd1b78a9c298
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-03-02 16:09:05 +0530

    Create vv55.c

---
+++ vv55.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+ int a,b,pro;
+ printf("enter the two numbers");
+ scanf("%d%d",&a,&b);
+ pro=a*b;
+ if(pro%2==0)
+ {
+ printf("%d",even);
+ }
+ else
+ {
+ printf("%d",odd);
+ }
+ }

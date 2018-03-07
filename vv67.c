65e7804c9870f117311bc5ca4f9640bfad5fd3bb
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2018-03-07 15:59:43 +0530

    Create vv67.c

---
+++ vv67.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    while(a%10!=0)
+    {
+        n++;
+    }
+    printf("nearest greater multiple of ten%d",n);
+}
